fca3d1d0ac0535d507538f65cc1336fa783ce259
Ando <108226527+andododo@users.noreply.github.com>
2024-03-04 07:39:37 +0800

    Delete raspi_python directory

--- raspi_python/microphone_list.py
+++
@@ -1,8 +0,0 @@
-import speech_recognition as sr
-
-def list_microphones():
-    microphones = sr.Microphone.list_microphone_names()
-    for i, mic_name in enumerate(microphones):
-        print(f"Microphone {i}: {mic_name}")
-
-list_microphones()
