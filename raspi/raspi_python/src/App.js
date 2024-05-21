c169d963c0258b3248006ca1e4756a1537c1088d
Ando <108226527+andododo@users.noreply.github.com>
2024-05-22 04:34:34 +0800

    not the main
    can't transfer the whole frontend folder for some reason

---
+++ raspi/raspi_python/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import VoiceAssistant from './VoiceAssistant';
+
+function App() {
+  return (
+    <div>
+      <VoiceAssistant />
+    </div>
+  );
+}
+
+export default App;
